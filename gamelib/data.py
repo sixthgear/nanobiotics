3ef4007ed16fc3f86400e205d32b31ac940089a4
sixthgear <sixthgear@sixthgear.ca>
2011-09-11 12:34:05 -0700

    Added data.py

---
+++ gamelib/data.py
@@ -0,0 +1,17 @@
+"""
+Please forgive me.
+"""
+
+import pyglet
+
+pyglet.resource.path = ['data']
+pyglet.resource.reindex()
+
+# pyglet.resource.add_font('unsteadyoversteer.ttf')
+# pyglet.font.load('Unsteady Oversteer')
+
+# spritesheet = pyglet.image.ImageGrid(pyglet.resource.image('sprites.png'),4,8).get_texture_sequence()
+# for sprite in spritesheet:
+#     sprite.anchor_x = sprite.width / 2 
+#     sprite.anchor_y = sprite.height / 2 
+
