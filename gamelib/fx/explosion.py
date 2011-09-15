f6b54f79681cfad542aa154f21c26f914177e97d
sixthgear <sixthgear@sixthgear.ca>
2011-09-15 01:07:01 -0700

    BIGGER EXPLODE

--- gamelib/fx/explosion.py
+++ gamelib/fx/explosion.py
@@ -24,22 +24,22 @@ class Exploder(object):
         
         self.fire = ParticleGroup(
         	controllers=[
-        		Lifetime(0.5),
-        		Movement(damping=0.96),
-        		Growth(20),
+        		Lifetime(1.50),
+        		Movement(damping=0.91),
+                Growth(10),
         		Fader(fade_in_start=0, start_alpha=0, fade_in_end=0.1, max_alpha=1.0, 
-        			fade_out_start=0.25, fade_out_end=0.5)
+        			fade_out_start=0.75, fade_out_end=1.50)
         	],
         	renderer=BillboardRenderer(SpriteTexturizer(fire_tex.id)))
 
         self.firer = StaticEmitter(
         	template=Particle(
         		position=(0,0,0), 
-        		size=(80,80,0)),
+        		size=(190,190,0)),
         	deviation=Particle(
-        		position=(10,10,0), 
-        		velocity=(60,60,0), 
-        		size=(15,15,0),
+        		position=(20,20,0), 
+        		velocity=(600,600,0), 
+        		size=(40,40,0),
                 # up=(0,0,math.pi*2), 
                 # rotation=(0,0,math.pi*0.06),
                 # age=2,
@@ -50,9 +50,9 @@ class Exploder(object):
         self.sparks = ParticleGroup(
           controllers=[
               # Gravity((0,-240,0)),
-              Lifetime(2),
+              Lifetime(1.2),
               Movement(damping=0.97),
-              Fader(fade_out_start=1.5, fade_out_end=2.0),
+              Fader(fade_out_start=0.75, fade_out_end=1.2),
           ],
           renderer=BillboardRenderer(SpriteTexturizer(spark_tex.id)))
         
@@ -62,16 +62,16 @@ class Exploder(object):
               color=(1,1,1)),
           deviation=Particle(
               position=(1,1,0), 
-              velocity=(400,400,0), 
-              age=1.5),
-          size=[(5,5,0), (6,6,0), (7,7,0)])
+              velocity=(600,600,0), 
+              ),
+          size=[(5,5,0), (7,7,0), (10,10,0)])
                        
     def explode(self, x, y, size=1.0, color=(1,1,1)):
         self.firer.template.position = (x,y,0)
-        self.firer.emit(16, self.fire)        
-        # self.sparker.template.position = (x,y,0)
-        # self.sparker.template.color = color
-        # self.sparker.emit(4, self.sparks)
+        self.firer.emit(120, self.fire)        
+        self.sparker.template.position = (x,y,0)
+        self.sparker.template.color = color
+        self.sparker.emit(120, self.sparks)
         # self.__class__.sound.play()
         
     def update(self, dt):
