88cdb5ae23cdb0a665bb2cbdd640013487a08f05
Michael Jard <mjard@misadventuregames.com>
2011-09-15 23:52:44 -0700

    these weapons are a lie

---
+++ gamelib/weapons/twinturret.py
@@ -0,0 +1,28 @@
+import pyglet
+
+from gamelib import bullet
+from weapon import Weapon
+
+class TwinTurret(Weapon):
+    
+    fire_sound = pyglet.resource.media('fire.wav', streaming=False)
+
+    def __init__(self, *args):
+        Weapon.__init__(self,*args)
+        self.cooldown = 5
+
+    def update(self, *args):
+        Weapon.update(self, *args)
+
+        if self.cooldown > 0:
+            self.cooldown -= 1
+            return
+
+        self.cooldown = 5
+
+        if self.engaged:
+            bv = (self.target_pos - self.pos).normal * 1400
+            bp = self.pos
+            if bv.x != 0 or bv.y != 0:
+                self.fire_sound.play()
+                bullet.pool.fire(bp.x, bp.y, bv.x, bv.y) 
